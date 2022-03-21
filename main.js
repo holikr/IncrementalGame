d7ff6a4d76062d39aa5509e5f1ed535143e64026
holikr <92577905+holikr@users.noreply.github.com>
2022-03-21 17:38:58 +0200

    Add files via upload
    Added assets (coin icons)
    Layout changes
    Changed some text & code from gold to coins

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
  }
 
 var gameData = {
-    gold: 0.0,
-    goldPerClickTotal: 1.0,
+    gold: 10.0,
+    goldPerClickTotal: 0.0,
     goldPerSecondTotal: 0.0,
     lastTick: Date.now(),
 
@@ -19,14 +19,14 @@ var gameData = {
 
     goldPerClickAxe: 0,
     goldPerSecondAxe: 0.0,
-    goldPerClickCostAxe: 1000,
+    goldPerClickCostAxe: 100,
     axeLevel: 0,
     axeTier: "Wooden Axe",
     axeMulti: 1,
 
     goldPerClickBow: 0,
     goldPerSecondBow: 0.0,
-    goldPerClickCostBow: 5000,
+    goldPerClickCostBow: 500,
     bowLevel: 0,
     bowTier: "Wooden Bow",
     bowMulti: 1
@@ -38,22 +38,22 @@ function update(id, content) {
 
 function grindGold() {
     gameData.gold += gameData.goldPerClickTotal
-    update("goldGround", format(gameData.gold, "scientific") + " Gold")
+    update("goldGround"," " + format(gameData.gold, "scientific")  + " Coins")
 }
 
 function buySwordLevel() {
     if (gameData.gold >= gameData.goldPerClickCostSword){
         gameData.gold -= gameData.goldPerClickCostSword
-        gameData.goldPerClickSword += (2.0 * gameData.swordMulti)
-        gameData.goldPerClickTotal += (2.0 * gameData.swordMulti)
-        gameData.goldPerClickCostSword *= 1.5
+        gameData.goldPerClickSword += (0.1 * gameData.swordMulti)
+        gameData.goldPerClickTotal += (0.1 * gameData.swordMulti)
+        gameData.goldPerClickCostSword *= 1.1
         gameData.swordLevel += 1
-        gameData.goldPerSecondSword += (2.0 * gameData.swordMulti)
-        gameData.goldPerSecondTotal += (2.0 * gameData.swordMulti)
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        gameData.goldPerSecondSword += (0.2 * gameData.swordMulti)
+        gameData.goldPerSecondTotal += (0.2 * gameData.swordMulti)
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("swordPerClickLevel", gameData.swordTier + " (Level " + gameData.swordLevel + ") Cost: " + format(gameData.goldPerClickCostSword, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         if (gameData.swordLevel >= 10) {
             document.getElementById("swordUpgrade").disabled = false
         }
@@ -61,9 +61,9 @@ function buySwordLevel() {
 }
 
 function buySwordUpgrade() {
-    if (gameData.gold >= 1000.0 & gameData.swordLevel >= 10) {
+    if (gameData.gold >= 100.0 & gameData.swordLevel >= 10) {
         gameData.swordTier = "Copper Sword"
-        gameData.gold -= 1000.0
+        gameData.gold -= 100.0
         gameData.swordMulti += 1
         if (gameData.goldPerClickSword != 0) {
             gameData.goldPerClickSword = gameData.goldPerClickSword * gameData.swordMulti
@@ -71,10 +71,10 @@ function buySwordUpgrade() {
         }
         gameData.goldPerSecondSword = gameData.goldPerSecondSword * gameData.swordMulti
         gameData.goldPerSecondTotal = gameData.goldPerSecondSword + gameData.goldPerSecondAxe + gameData.goldPerSecondBow
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("swordPerClickLevel", gameData.swordTier + " (Level " + gameData.swordLevel + ") Cost: " + format(gameData.goldPerClickCostSword, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         document.getElementById("swordUpgrade").remove()
      }
  }
@@ -82,16 +82,16 @@ function buySwordUpgrade() {
 function buyAxeLevel() {
     if (gameData.gold >= gameData.goldPerClickCostAxe){
         gameData.gold -= gameData.goldPerClickCostAxe
-        gameData.goldPerClickAxe += (5.0 * gameData.axeMulti)
-        gameData.goldPerClickTotal += (5.0 * gameData.axeMulti)
-        gameData.goldPerClickCostAxe *= 1.5
+        gameData.goldPerClickAxe += (0.2 * gameData.axeMulti)
+        gameData.goldPerClickTotal += (0.2 * gameData.axeMulti)
+        gameData.goldPerClickCostAxe *= 1.1
         gameData.axeLevel += 1
-        gameData.goldPerSecondAxe += (5.0 * gameData.axeMulti)
-        gameData.goldPerSecondTotal += (5.0 * gameData.axeMulti)
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        gameData.goldPerSecondAxe += (0.4 * gameData.axeMulti)
+        gameData.goldPerSecondTotal += (0.4 * gameData.axeMulti)
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("axePerClickLevel", gameData.axeTier + " (Level " + gameData.axeLevel + ") Cost: " + format(gameData.goldPerClickCostAxe, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         if (gameData.axeLevel >= 10) {
             document.getElementById("axeUpgrade").disabled = false
         }
@@ -99,9 +99,9 @@ function buyAxeLevel() {
 }
 
 function buyAxeUpgrade() {
-    if (gameData.gold >= 50000.0 & gameData.axeLevel >= 10) {
+    if (gameData.gold >= 1000.0 & gameData.axeLevel >= 10) {
         gameData.axeTier = "Copper Axe"
-        gameData.gold -= 50000.0
+        gameData.gold -= 1000.0
         gameData.axeMulti += 1
         if (gameData.goldPerClickAxe != 0) {
             gameData.goldPerClickAxe = gameData.goldPerClickAxe * gameData.axeMulti
@@ -109,10 +109,10 @@ function buyAxeUpgrade() {
         }
         gameData.goldPerSecondAxe = gameData.goldPerSecondAxe * gameData.axeMulti
         gameData.goldPerSecondTotal = gameData.goldPerSecondSword + gameData.goldPerSecondAxe + gameData.goldPerSecondBow
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("axePerClickLevel", gameData.axeTier + " (Level " + gameData.axeLevel + ") Cost: " + format(gameData.goldPerClickCostAxe, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         document.getElementById("axeUpgrade").remove()
      }
  }
@@ -120,16 +120,16 @@ function buyAxeUpgrade() {
 function buyBowLevel() {
     if (gameData.gold >= gameData.goldPerClickCostBow){
         gameData.gold -= gameData.goldPerClickCostBow
-        gameData.goldPerClickBow += (10.0 *gameData.bowMulti)
-        gameData.goldPerClickTotal += (10.0 * gameData.bowMulti)
-        gameData.goldPerClickCostBow *= 1.5
+        gameData.goldPerClickBow += (0.5 *gameData.bowMulti)
+        gameData.goldPerClickTotal += (0.5 * gameData.bowMulti)
+        gameData.goldPerClickCostBow *= 1.1
         gameData.bowLevel += 1
-        gameData.goldPerSecondBow += (10.0 *gameData.bowMulti)
-        gameData.goldPerSecondTotal += (10.0 *gameData.bowMulti)
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        gameData.goldPerSecondBow += (1.0 *gameData.bowMulti)
+        gameData.goldPerSecondTotal += (1.0 *gameData.bowMulti)
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("bowPerClickLevel", gameData.bowTier + " (Level " + gameData.bowLevel + ") Cost: " + format(gameData.goldPerClickCostBow, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         if (gameData.bowLevel >= 10) {
             document.getElementById("bowUpgrade").disabled = false
         }
@@ -137,9 +137,9 @@ function buyBowLevel() {
 }
 
 function buyBowUpgrade() {
-    if (gameData.gold >= 75000.0 & gameData.bowLevel >= 10) {
+    if (gameData.gold >= 5000.0 & gameData.bowLevel >= 10) {
         gameData.bowTier = "Copper Bow"
-        gameData.gold -= 75000.0
+        gameData.gold -= 5000.0
         gameData.bowMulti += 1
         if (gameData.goldPerClickBow != 0) {
             gameData.goldPerClickBow = gameData.goldPerClickBow * gameData.bowMulti
@@ -147,21 +147,30 @@ function buyBowUpgrade() {
         }
         gameData.goldPerSecondBow = gameData.goldPerSecondBow * gameData.bowMulti
         gameData.goldPerSecondTotal = gameData.goldPerSecondSword + gameData.goldPerSecondAxe + gameData.goldPerSecondBow
-        update("goldGround", format(gameData.gold, "scientific") + " Gold")
+        update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
         update("bowPerClickLevel", gameData.bowTier + " (Level " + gameData.bowLevel + ") Cost: " + format(gameData.goldPerClickCostBow, "scientific") + " Gold")
-        update("goldPerSecondTotal", format(gameData.goldPerSecondTotal, "scientific") + " Gold Per Second")
-        update("goldPerClickTotal", format(gameData.goldPerClickTotal, "scientific") + " Gold Per Click")
+        update("goldPerSecondTotal", " " + format(gameData.goldPerSecondTotal, "scientific") + " Per Second")
+        update("goldPerClickTotal", " " + format(gameData.goldPerClickTotal, "scientific") + " Per Click")
         document.getElementById("bowUpgrade").remove()
-     }
- }
+    }
+}
 
 //pelin tickeri millisekunteina
 var mainGameLoop = window.setInterval(function() {
     diff = Date.now() - gameData.lastTick;
     gameData.lastTick = Date.now()
     gameData.gold += gameData.goldPerSecondTotal
-    update("goldGround", format(gameData.gold, "scientific") + " Gold")
-  }, 1000)
+    update("goldGround", " " + format(gameData.gold, "scientific") + " Coins")
+}, 1000)
+
+function tab(tab) {
+    // hide all your tabs, then show the one the user selected.
+    document.getElementById("itemsMenu").style.display = "none"
+    document.getElementById("upgradesMenu").style.display = "none"
+    document.getElementById(tab).style.display = "initial"
+  }
+// go to a tab for the first time, so not all show
+tab("itemsMenu")
 
 //pelin tallennus
 var saveGameLoop = window.setInterval(function() {
